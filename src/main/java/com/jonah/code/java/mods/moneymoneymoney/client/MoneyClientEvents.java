589ff8408a4441378d61aa7e21ac44c90b0f30e6
itsagorski1 <jonahisaacgorski73@gmail.com>
2026-04-01 16:33:20 -0700

    add bank

--- src/main/java/com/jonah/code/java/mods/moneymoneymoney/client/MoneyClientEvents.java
+++ src/main/java/com/jonah/code/java/mods/moneymoneymoney/client/MoneyClientEvents.java
@@ -4,10 +4,7 @@ import com.jonah.code.java.mods.moneymoneymoney.MoneyMoneyMoneyMod;
 import com.jonah.code.java.mods.moneymoneymoney.client.render.BankerRenderer;
 import com.jonah.code.java.mods.moneymoneymoney.registry.ModEntityTypes;
 import com.jonah.code.java.mods.moneymoneymoney.registry.ModMenus;
-<<<<<<< HEAD
 import net.minecraft.client.renderer.entity.EntityRenderers;
-=======
->>>>>>> f7408f5c75d29156775a186b0a8595500a763f43
 import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -21,13 +18,9 @@ public final class MoneyClientEvents {
 
     @SubscribeEvent
     public static void onClientSetup(FMLClientSetupEvent event) {
-<<<<<<< HEAD
         event.enqueueWork(() -> {
             MenuScreens.register(ModMenus.MONEY_MENU.get(), MoneyScreen::new);
             EntityRenderers.register(ModEntityTypes.BANKER.get(), BankerRenderer::new);
         });
-=======
-        event.enqueueWork(() -> MenuScreens.register(ModMenus.MONEY_MENU.get(), MoneyScreen::new));
->>>>>>> f7408f5c75d29156775a186b0a8595500a763f43
     }
 }
