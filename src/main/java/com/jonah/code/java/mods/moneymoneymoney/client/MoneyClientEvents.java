bfed0181b2faae5ab9af6989c344d42007388395
itsagorski1 <jonahisaacgorski73@gmail.com>
2026-04-10 17:11:19 -0700

    fix

--- src/main/java/com/jonah/code/java/mods/moneymoneymoney/client/MoneyClientEvents.java
+++ src/main/java/com/jonah/code/java/mods/moneymoneymoney/client/MoneyClientEvents.java
@@ -2,10 +2,11 @@ package com.jonah.code.java.mods.moneymoneymoney.client;
 
 import com.jonah.code.java.mods.moneymoneymoney.MoneyMoneyMoneyMod;
 import com.jonah.code.java.mods.moneymoneymoney.registry.ModMenus;
+import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.RegisterMenuScreensEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 @Mod.EventBusSubscriber(modid = MoneyMoneyMoneyMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public final class MoneyClientEvents {
@@ -13,7 +14,7 @@ public final class MoneyClientEvents {
     }
 
     @SubscribeEvent
-    public static void onRegisterScreens(RegisterMenuScreensEvent event) {
-        event.register(ModMenus.MONEY_MENU.get(), MoneyScreen::new);
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        event.enqueueWork(() -> MenuScreens.register(ModMenus.MONEY_MENU.get(), MoneyScreen::new));
     }
 }
